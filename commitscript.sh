e4559f997e3c098bcd9f7f8f62247f92a3571caf
sushmakethinenii <kethineni.sushma1520@gmail.com>
2024-01-05 12:44:24 -0600

    JIRA-143

--- commitscript.sh
+++ commitscript.sh
@@ -1,4 +1,4 @@
-### Replace placeholders with your GitHub username, repository name, and optional parameters
+
 #USERNAME="sushmakethinenii"
 #REPO="devops"
 #ACCESS_TOKEN="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"
@@ -8,9 +8,9 @@
 # Replace placeholders with your GitHub username, repository name, access token, and Jira ticket number
 
 USERNAME="sushmakethinenii"
-iREPO="devops"
+REPO="Devops"
 ACCESS_TOKEN="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"
-JIRA_TICKET="100"
+JIRA_TICKET="/JIRA-123"
 
 # Fetch commits using the GitHub REST API with a search parameter
 curl -u "$USERNAME:$ACCESS_TOKEN" "https://api.github.com/repos/$USERNAME/$REPO/commits?grep=$JIRA_TICKET"
